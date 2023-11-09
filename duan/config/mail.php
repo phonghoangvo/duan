c240bc4c4d94c68b688de5190490cb09a6b5befb
Dangwingg <106939343+Dangwingg@users.noreply.github.com>
2023-11-09 20:48:53 +0700

    Update mail.php

--- duan/config/mail.php
+++ duan/config/mail.php
@@ -38,6 +38,7 @@ return [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => 'fightingtogether06@gmail.com',
