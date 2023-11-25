e866ad7d359fe1350d674257a66a8358e4b235fc
phonghoangvo <phongpro188@gmail.com>
2023-11-25 21:08:33 +0700

    logo

--- duan/resources/views/navbar.blade.php
+++ duan/resources/views/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="container px-0">
             <!-- Logo -->
             <a class="navbar-brand" href="/">
-                <img src="/img/logo.png" alt="" style="width: 180px;">
+                <img src="/img/logo.jpg" alt="" style="width: 180px; height:80px">
             </a>
             <!-- Toggle Btn -->
             <button class="navbar-toggler shadow-none border-" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
