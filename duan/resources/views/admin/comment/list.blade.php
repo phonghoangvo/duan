2e787e1bfa2d5cefad560b302f79d3778add7d92
Dangwingg <106939343+Dangwingg@users.noreply.github.com>
2023-12-01 23:56:52 +0700

    Update list.blade.php

--- duan/resources/views/admin/comment/list.blade.php
+++ duan/resources/views/admin/comment/list.blade.php
@@ -4,40 +4,58 @@ Dashboard - Quản Trị Website
 @endsection
 
 @section('noidung')
-                <div class="container">
+<style>
+    .limited-text {
+        max-width: 200px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+</style>
+                <div class="container-fluid">
                     <h2 class="mt-4 mb-4">Danh sách bình luận - Admin</h2>
                     @if(Session::has('success'))
                     <div class="alert alert-success" role="alert">
                         {{Session::get('success')}}
                     </div>
                     @endif
-                    <table class="table table-striped">
-                        <thead  style="background-color: #1a5fc7; text-align:center;color:white">
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body p-0">
+                                  <div class="table-responsive">
+                                    <table class="table table-striped mb-0 text-center">
+                        <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>ID user</th>
-                                <th>ID Product</th>
-                                <th>Content</th>
-                                <th>Time</th>
-                                <th>Chức Năng</th>
+                                <th scope="col" class="col-lg-1">ID</th>
+                                <th scope="col" class="col-lg-1">ID user</th>
+                                <th scope="col" class="col-lg-1">ID Product</th>
+                                <th scope="col" class="col-lg-1">Content</th>
+                                <th scope="col" class="col-lg-1">Time</th>
+                                <th scope="col" class="col-lg-1">Chức Năng</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($comment as $comment)
                             <tr>
-                                <td>{{$comment->id}}</td>
-                                <td>{{$comment->id_user}}</td>
-                                <td>{{$comment->id_product}}</td>
-                                <td>{{$comment->content}}</td>
-                                <td>{{$comment->time}}</td>
+                                <td class="limited-text">{{$comment->id}}</td>
+                                <td class="limited-text">{{$comment->id_user}}</td>
+                                <td class="limited-text">{{$comment->id_product}}</td>
+                                <td class="limited-text">{{$comment->content}}</td>
+                                <td class="limited-text">{{$comment->time}}</td>
                                 <td>
                                    <center> <a href="{{url('del/'.$comment->id)}}"
-                                        onclick="return confirm('Ban muốn xóa bình luận ?')" title="Xoa"><button
-                                            type="button" class="btn btn-info">Xóa</button></a></center>
+                                        onclick="return confirm('Bạn muốn xóa bình luận ?')" title="Xoa"><button
+                                            type="button" class="btn btn-danger w-100 mb-2">Xóa</button></a></center>
                                 </td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </div>
-                @endsection
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
