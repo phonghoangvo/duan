46713ea10ffab0d4bcbe5a23ba869fa525fdb46c
Dangwingg <106939343+Dangwingg@users.noreply.github.com>
2023-12-01 23:59:36 +0700

    Update edit.blade.php

--- duan/resources/views/admin/sanpham/edit.blade.php
+++ duan/resources/views/admin/sanpham/edit.blade.php
@@ -4,7 +4,111 @@ Dashboard - Quản Trị Website
 @endsection
 
 @section('noidung')
-<div class="container-fluid" style="background: white">
+<div class="mask d-flex align-items-center h-100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <div class="card" style="border-radius: 1rem;">
+                    <div class="card-body p-5">
+                        @if(Session::has('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{Session::get('success')}}
+                        </div>
+                        @endif
+                        <form action="{{url('update/'.$product->id)}}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <!-- 2 column grid layout with text inputs for the first and last names -->
+                            <div class="row">
+                                <div class="col-12 col-md-6 mb-4">
+                                    <div class="form-outline">
+                                        <input type="hidden" name="id" value="{{$product->id}}">
+                                        <label for="name" class="formbold-form-label"> Tên Sản Phẩm </label>
+                                        <input type="text" name="name" id="name" placeholder="Nhập Tên Sản Phẩm"
+                                            class="form-control" value="{{$product->name}}" />
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6 mb-4">
+                                    <div class="form-outline">
+                                        <label for="price" class="formbold-form-label"> Giá Sản Phẩm </label>
+                                        <input type="number" name="price" id="price" placeholder="Nhập Giá Sản Phẩm"
+                                            class="form-control" value="{{$product->price}}" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <label for="phone" class="formbold-form-label"> Giảm giá </label>
+                                <input type="number" name="priceSale" id="priceSale"
+                                    placeholder="Nhập Giảm giá (nếu có)" class="form-control"
+                                    value="{{$product->priceSale}}" />
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <label for="name" class="formbold-form-label"> Nhà cung cấp</label>
+                                <input type="text" name="nhacungcap" value="{{$product->nhacungcap}}" id="nhacungcap"
+                                    placeholder="Nhập Nhà Cung Cấp" class="form-control" />
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <label for="name" class="formbold-form-label"> Nhà xuất bản </label>
+                                <input type="text" name="nhaxuatban" id="nhaxuatban" value="{{$product->nhaxuatban}}"
+                                    placeholder="Nhập nhà xuất bản" class="form-control" />
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <label for="name" class="formbold-form-label"> Tác giả</label>
+                                <input type="text" name="tacgia" id="tacgia" value="{{$product->tacgia}}"
+                                    placeholder="Nhập Tên tác giả" class="form-control" />
+                            </div>
+                            <div class="form-outline mb-4">
+                                <label for="name" class="formbold-form-label"> Ngày đăng </label>
+                                <input type="date" name="ngayDang" id="ngayDang" value="{{$product->ngayDang}}"
+                                    placeholder="Nhập ngày đăng" class="form-control" />
+                            </div>
+
+                            <!-- Message input -->
+                            <div class="form-outline mb-4">
+                                <label for="message" class="formbold-form-label"> Mô tả sản phẩm </label>
+                                <textarea rows="6" name="moTa" id="moTa" placeholder="Viết mô tả sản phẩm"
+                                    class="form-control">
+                    {{$product->moTa}}
+                </textarea>
+                            </div>
+                            <div class="formbold-form-label">
+                                <label>Danh mục:</label><br>
+                                <select name="idCategory" required class="form-control">
+                                    <option value="1">--Danh Mục--</option>
+                                    @foreach ($category as $category)
+                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div><br>
+                            <label for="upload" class="formbold-input-label">
+                                Thêm Ảnh Sản Phẩm
+
+                                <input type="file" name="img" value="{{$product->img}}" class="form-control"
+                                    id="customFile" />
+                            </label>
+                            @if($product->img)
+                            <img src="{{$product->img}}">
+                            @endif
+                            <div class="button">
+                            <input type="submit" value="Cập Nhật" class="formbold-btn">
+                            <button class="formbold-btn">
+                                <a style="text-decoration: none;color:white" href="{{url('admin/list')}}">Danh sách sản
+                                    phẩm </a>
+                            </button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <div class="container-fluid" style="background: white">
     <div class="formbold-main-wrapper">
         <div class="formbold-form-wrapper">
             @if(Session::has('success'))
@@ -19,7 +123,7 @@ Dashboard - Quản Trị Website
                     <div>
                         <input type="hidden" name="id" value="{{$product->id}}">
                         <input type="text" style="width:550px" name="name" id="name" placeholder="Nhập Tên Sản Phẩm"
-                            class="formbold-form-input" value="{{$product->name}}" />
+                        class="form-control" value="{{$product->name}}" />
                         <label for="name" class="formbold-form-label"> Tên Sản Phẩm </label>
                     </div>
                 </div>
@@ -27,45 +131,45 @@ Dashboard - Quản Trị Website
                 <div class="formbold-input-flex">
                     <div>
                         <input type="number" name="price" id="price" placeholder="Nhập Giá Sản Phẩm"
-                            class="formbold-form-input" value="{{$product->price}}" />
+                            class="form-control" value="{{$product->price}}" />
                         <label for="email" class="formbold-form-label"> Giá Sản Phẩm </label>
                     </div>
                     <div>
                         <input type="number" name="priceSale" id="priceSale" placeholder="Nhập Giảm giá (nếu có)"
-                            class="formbold-form-input" value="{{$product->priceSale}}" />
+                            class="form-control" value="{{$product->priceSale}}" />
                         <label for="phone" class="formbold-form-label"> Giảm giá </label>
                     </div>
                 </div>
                 <div class="formbold-input-flex">
                     <div>
                         <input type="text" name="nhacungcap" value="{{$product->nhacungcap}}" id="nhacungcap"
-                            placeholder="Nhập Nhà Cung Cấp" class="formbold-form-input" />
+                            placeholder="Nhập Nhà Cung Cấp" class="form-control" />
                         <label for="name" class="formbold-form-label"> Nhà cung cấp</label>
                     </div>
                     <div>
                         <input type="text" name="nhaxuatban" id="nhaxuatban" value="{{$product->nhaxuatban}}"
-                            placeholder="Nhập nhà xuất bản" class="formbold-form-input" />
+                            placeholder="Nhập nhà xuất bản" class="form-control" />
                         <label for="name" class="formbold-form-label"> Nhà xuất bản </label>
                     </div>
                 </div>
                 <div class="formbold-input-flex">
                     <div>
                         <input type="text" name="tacgia" id="tacgia" value="{{$product->tacgia}}"
-                            placeholder="Nhập Tên tác giả" class="formbold-form-input" />
+                            placeholder="Nhập Tên tác giả" class="form-control" />
                         <label for="name" class="formbold-form-label"> Tác giả</label>
                     </div>
                     <div>
                         <input type="date" name="ngayDang" id="ngayDang" value="{{$product->ngayDang}}"
-                            placeholder="Nhập ngày đăng" class="formbold-form-input" />
+                            placeholder="Nhập ngày đăng" class="form-control" />
                         <label for="name" class="formbold-form-label"> Ngày đăng </label>
                     </div>
                 </div>
                 <div class="formbold-textarea">
                     <textarea rows="6" name="moTa" id="moTa" placeholder="Viết tóm tắt sản phẩm"
-                        class="formbold-form-input">
+                        class="form-control">
                         {{$product->moTa}}
                     </textarea>
-                    <label for="message" class="formbold-form-label"> Tóm tắt sản phẩm </label>
+                    <label for="message" class="formbold-form-label"> Mô tả sản phẩm </label>
                 </div>
                 <div class="formbold-form-label">
                     <label>Danh mục:</label><br>
@@ -94,5 +198,5 @@ Dashboard - Quản Trị Website
             </form>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
