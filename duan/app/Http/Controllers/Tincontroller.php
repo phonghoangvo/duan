d6d673fe97c37b3f56171634176dcc33f37599e8
phucnon <phuc1235342926@gmail.com>
2023-12-17 20:49:27 +0700

    gvccg
    1111

--- duan/app/Http/Controllers/Tincontroller.php
+++ duan/app/Http/Controllers/Tincontroller.php
@@ -203,7 +203,9 @@ public function timkiem(Request $request)
         return view('admin.listtintuc', compact('listtintuc'));
     }
 
-   
+    function admin() {
+        return view('admin.admin');
+    }
 
     function themtin() {
         return view('admin.tin.themtin');
