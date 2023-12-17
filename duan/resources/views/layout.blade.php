6ba341568d61e0beefba6cc3efd0ba768cf0f40d
phonghoangvo <phongpro188@gmail.com>
2023-12-18 00:06:27 +0700

    1

--- duan/resources/views/layout.blade.php
+++ duan/resources/views/layout.blade.php
@@ -65,6 +65,8 @@
           document.documentElement.scrollTop = 0;
         }
         </script>
+<<<<<<< HEAD
+<<<<<<< HEAD
       <script type="text/javascript">
   $(".cart-update").change(function (e) {
     e.preventDefault();
@@ -119,5 +121,49 @@
     });
 </script>
 
+=======
+=======
+>>>>>>> parent of 2dc8eba (1)
+        <script type="text/javascript">
+          $(".cart-update").change(function (e){
+              e.preventDefault();
+              var ele = $(this);
+              $.ajax({
+                  url: '{{ route('update_cart') }}',
+                  method: "patch",
+                  data: {
+                      _token: '{{ csrf_token() }}',
+                      id: ele.parents("tr").attr("data-id"),
+                      quanlity: ele.parents("tr").find(".quanlity").val()
+                  },
+                  success: function(response){
+                      window.location.reload();
+                  }
+              });
+          });
+  
+          $(".cart-remove").click(function(e){
+              e.preventDefault();
+              var ele = $(this);
+              if(confirm("Bạn có muốn xóa sản phẩm khỏi giỏ hàng không?")){
+                  $.ajax({
+                      url: '{{ route('remove_from_cart') }}',
+                      method: "DELETE",
+                      data: {
+                          _token: '{{ csrf_token() }}',
+                          id: ele.parents("tr").attr("data-id")
+                      },
+                      success: function (response) {
+                          window.location.reload();
+                      }
+                  });
+              }
+          });
+      </script>
+  
+<<<<<<< HEAD
+>>>>>>> parent of 2dc8eba (1)
+=======
+>>>>>>> parent of 2dc8eba (1)
 </body>
 </html>
