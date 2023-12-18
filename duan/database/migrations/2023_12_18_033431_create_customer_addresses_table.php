57e7f2ca73303b1dd4ad6a1085f1a3a2099791f0
phonghoangvo <phongpro188@gmail.com>
2023-12-18 20:20:05 +0700

    .

--- duan/database/migrations/2023_12_18_033431_create_customer_addresses_table.php
+++ duan/database/migrations/2023_12_18_033431_create_customer_addresses_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<<< HEAD:duan/database/migrations/2023_12_18_033431_create_customer_addresses_table.php
         Schema::create('customer_addresses', function (Blueprint $table) {
             $table->id();
             $table->foreignId('users')->constrained()->onDelete('cascade');
@@ -21,14 +20,6 @@ return new class extends Migration
             $table->string('soDienThoai');
             $table->string('diaChi');
             $table->string('phuongthucthanhtoan');
-========
-        Schema::create('categories', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
->>>>>>>> main:duan/database/migrations/2023_10_09_095402_categorie.php
-            $table->timestamps();
-            $table->string('name',50);
-            $table->tinyInteger('hidden')->default(1);
-            $table->string('slug',100);
         });
     }
 
@@ -37,10 +28,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-<<<<<<<< HEAD:duan/database/migrations/2023_12_18_033431_create_customer_addresses_table.php
         Schema::dropIfExists('customer_addresses');
-========
-        Schema::dropIfExists('categories');
->>>>>>>> main:duan/database/migrations/2023_10_09_095402_categorie.php
+
     }
 };
